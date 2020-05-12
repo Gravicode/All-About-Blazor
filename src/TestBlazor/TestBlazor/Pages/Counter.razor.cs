f4c10d28777f89eb6ea99fd42d2bd9cc8cfade46
Mif Masterz <mifmasterz@yahoo.com>
2020-05-12 15:52:57 +0700

    autocomplete

--- src/TestBlazor/TestBlazor/Pages/Counter.razor.cs
+++ src/TestBlazor/TestBlazor/Pages/Counter.razor.cs
@@ -2,14 +2,24 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
 
 namespace TestBlazor.Pages
 {
     public partial class Counter
     {
+        [Parameter]
+        public int newcount { get; set; }
         public string message { set; get; }
-        int currentCount = 0;
-
+        [Parameter]
+        public int currentCount {set;get;} = 0;
+protected override async Task OnInitializedAsync()
+    {
+        currentCount+=newcount;
+        //var data = new List<int>{1,2,3,4,5,6,7};
+        //Cetak<int>(data);
+    }
         void IncrementCount()
         {
             currentCount++;
@@ -19,6 +29,11 @@ namespace TestBlazor.Pages
         public static Task<string> ReturnData()
         {
             return Task.FromResult("Hello say...");
+        }
+
+        void Cetak<T>(List<T> Data){
+            foreach(var item in Data)
+                Console.WriteLine(item);
         }
     }
     
